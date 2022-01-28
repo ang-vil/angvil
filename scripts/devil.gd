406c4af645f5e83ac0fd8c1ca4325370a502b63b
EduardBaer <EduardBaer@users.noreply.github.com>
2022-01-28 18:17:04 +0100

    Make characters movable

--- scripts/devil.gd
+++ scripts/devil.gd
@@ -1,5 +1,21 @@
 extends KinematicBody2D
 
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("key_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("key_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("key_down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("key_up"):
+		velocity.y += 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	move_and_collide(Vector2(0, -1)) # Move down 1 pixel per physics frame
+	get_input()
+	velocity = move_and_slide(velocity)
