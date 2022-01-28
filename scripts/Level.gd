a148fbfefc1a931fa1aa0436e8b80f482b34a117
Michael Grüb <grueb.m@googlemail.com>
2022-01-28 22:27:29 +0100

    add escape key mapping to Level scene

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var keys_typed = []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +15,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+#Input handler, listen for ESC to exit app
+func _input(event):
+	# Record keys.
+	if event is InputEventKey and event.pressed and !event.echo:
+		keys_typed.append(OS.get_scancode_string(event.scancode))
+		# Quit on Escape press.
+		if event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+	# Quit on any mouse click.
+	# if event is InputEventMouseButton:
+	#	quit = true
