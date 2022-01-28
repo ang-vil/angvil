302d0edf2ce68248a3d1f24881c73e82473cb094
Bernd Stellwag <burned@zerties.org>
2022-01-28 19:52:20 +0100

    refactor ci

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ rm -rf dist/
 mkdir -p dist
 
 echo 'Building for web ...'
-godot --export 'HTML5' dist/index.html
+godot --export "HTML5" dist/index.html
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --export 'Linux/X11' dist/angvil-linux.x86_64
-chmod a+x dist/angvil-linux.x86_64
+godot --export 'Linux/X11' dist/angvil.x86_64
+chmod a+x dist/angvil.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --export 'Windows Desktop' dist/angvil-windows.exe
+godot --export 'Windows Desktop' dist/angvil.exe
 echo 'done.'
 
 echo 'done.'
