6d0f70de545f2f54ba83cd9ddd22ffd33f6b3883
Thomas Christlieb <thomaschristlieb@hotmail.com>
2022-01-29 21:52:13 +0100

    fix score and touchscreen controls not beeing on right bottom

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,11 +1,7 @@
 extends CanvasLayer
 
-func _on_viewport_size_changed():
-	$Control.set_h_size_flags(Control.SIZE_EXPAND_FILL)
-	$Control.set_anchors_preset(Control.PRESET_WIDE)
-	$Control.set_anchors_and_margins_preset(Control.PRESET_TOP_WIDE)
-
 func _ready():
-	$Control.set_h_size_flags(Control.SIZE_EXPAND_FILL)
-	$Control.set_anchors_preset(Control.PRESET_WIDE)
-	$Control.set_anchors_and_margins_preset(Control.PRESET_TOP_WIDE)
+	if OS.has_touchscreen_ui_hint():
+		OS.hide_virtual_keyboard()
+		$touchButtons.visible = true
+		
