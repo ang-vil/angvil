f4c946cda57a7d4ae6b1c7db62504c1b7552b7c9
Hayajiro <ciapa@lewd.wtf>
2022-01-29 14:32:03 +0100

    Disable mid-air direction changing

--- scripts/angel.gd
+++ scripts/angel.gd
@@ -5,20 +5,26 @@ var math = preload("res://scripts/helper/math.gd")
 
 var velocity = Vector2()
 var jumping = false
+var canMoveInAir = false
 
 func get_input(delta):
 	if Input.is_action_pressed("key_right"):
-		get_node("Sprite").set_flip_h(false)
-		velocity.x = lerp(velocity.x, vars.WALK_SPEED, 0.3)
+		if velocity.y == 0 or canMoveInAir:
+			canMoveInAir = false
+			get_node("Sprite").set_flip_h(false)
+			velocity.x = lerp(velocity.x, vars.WALK_SPEED, 0.3)
 	elif Input.is_action_pressed("key_left"):
-		get_node("Sprite").set_flip_h(true)
-		velocity.x = lerp(velocity.x, vars.WALK_SPEED*-1, 0.3)
+		if velocity.y == 0 or canMoveInAir:
+			canMoveInAir = false
+			get_node("Sprite").set_flip_h(true)
+			velocity.x = lerp(velocity.x, vars.WALK_SPEED*-1, 0.3)
 	else:
 		# smoothen walk
 		velocity.x = lerp(velocity.x, 0, 0.1)
 
 	if Input.is_action_just_pressed("key_up") and velocity.y == 0:
-		jumping = true;
+		jumping = true
+		canMoveInAir = true
 		$jumpsound.play()
 
 	if jumping:
