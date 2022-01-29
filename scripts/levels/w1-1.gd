faf45f732488f77fb30df4ce8aac4abe6d3225ac
Hayajiro <ciapa@lewd.wtf>
2022-01-29 13:59:43 +0100

    Implement level script

---
+++ scripts/levels/w1-1.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	# Record keys.
+	if event is InputEventKey and event.pressed and !event.echo:
+		# Quit on Escape press.
+		if event.scancode == KEY_ESCAPE:
+			get_tree().quit()
