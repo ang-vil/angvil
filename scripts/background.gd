5d548f4df2db19e3eb301afc3fc53a82491eac4b
Michael Grüb <grueb.m@googlemail.com>
2022-01-30 13:18:56 +0100

    prepere for autoload gobals

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,11 +1,5 @@
 extends ParallaxBackground
 
-const HEAVEN_COLOR = '#9dd1e7'
-const HELL_COLOR = '#833a38'
-
-var colorHeaven = '#9dd1e7'
-var colorHell = '#833a38'
-
 onready var heavenNode = get_node("ParallaxLayer/ColorHeaven")
 onready var hellNode = get_node("ParallaxLayer/ColorHell")
 
@@ -15,5 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	heavenNode.set_frame_color(Color(colorHeaven))
-	hellNode.set_frame_color(Color(colorHell))
+	heavenNode.set_frame_color(Color(COLOR_HEAVEN))
+	hellNode.set_frame_color(Color(COLOR_HELL))
