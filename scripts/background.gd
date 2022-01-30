d911328c2e85b957810b7634d3fcd63ddea4d59e
Michael Grüb <grueb.m@googlemail.com>
2022-01-30 13:33:55 +0100

    fix var

--- scripts/background.gd
+++ scripts/background.gd
@@ -3,8 +3,8 @@ extends ParallaxBackground
 const COLOR_HEAVEN = '#9dd1e7'
 const COLOR_HELL = '#833a38'
 
-var colorHeaven = '#9dd1e7'
-var colorHell = '#833a38'
+var colorHeaven = COLOR_HEAVEN
+var colorHell = COLOR_HELL
 
 onready var heavenNode = get_node("ParallaxLayer/ColorHeaven")
 onready var hellNode = get_node("ParallaxLayer/ColorHell")
@@ -15,5 +15,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	heavenNode.set_frame_color(Color(COLOR_HEAVEN))
-	hellNode.set_frame_color(Color(COLOR_HELL))
+	heavenNode.set_frame_color(Color(colorHeaven))
+	hellNode.set_frame_color(Color(colorHell))
